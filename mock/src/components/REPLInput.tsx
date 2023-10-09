6e430eb7f2ddb6f7c4dcf93e923d8d3bce073df4
Nicole Sanchez-Soto <nicole_sanchez-soto@brown.edu>
2023-10-09 06:08:46 -0400

    feat: implemented more detailed messages into history when running commands

--- mock/src/components/REPLInput.tsx
+++ mock/src/components/REPLInput.tsx
@@ -10,6 +10,7 @@ interface REPLInputProps {
 
 let brief: boolean = true;
 const dataMap = new Map([["simple.csv", parsedCSV]]);
+let currentData: string[][] | null = null;
 
 export function REPLInput(props: REPLInputProps) {
   const [commandString, setCommandString] = useState<string>("");
@@ -22,21 +23,49 @@ export function REPLInput(props: REPLInputProps) {
       );
     } else if (commandString.startsWith("load_file")) {
       const path = commandString.split(" ")[1];
-      const currentData = dataMap.get(path) || null;
+      currentData = dataMap.get(path) || null;
       if (currentData) {
-        props.addToHistory(<p>{`Loaded data from ${path}`}</p>);
+        if (brief) {
+          props.addToHistory(<p>{`Loaded data from ${path}`}</p>);
+        } else {
+          props.addToHistory(
+            <>
+              <p>{`Command: ${commandString}`}</p>
+              <p>{`Output: Loaded data from ${path}`}</p>
+            </>
+          );
+        }
       } else {
         props.addToHistory(<p>{"Invalid file path provided"}</p>);
       }
     } else if (commandString === "view") {
-      const table = createTable(dataMap.get("simple.csv")!);
+      if (currentData) {
+        const table = createTable(currentData);
+        if (brief) {
+          props.addToHistory(table);
+        } else {
+          props.addToHistory(
+            <>
+              <p>{`Command: ${commandString}`}</p>
+              <p>{`Output:`}</p>
+              {table}
+            </>
+          );
+        }
+      } else {
+        props.addToHistory(<p>No CSV loaded to view</p>);
+      }
+    } else if (commandString.startsWith("search")) {
+      const [, col, value] = commandString.split(" ");
+      const searchResult = searchTable(col, value);
       if (brief) {
-        props.addToHistory(table);
+        props.addToHistory(searchResult);
       } else {
         props.addToHistory(
           <>
             <p>{`Command: ${commandString}`}</p>
-            {table}
+            <p>{`Output:`}</p>
+            {searchResult}
           </>
         );
       }
@@ -72,6 +101,59 @@ export function REPLInput(props: REPLInputProps) {
         </thead>
         <tbody>
           {data.slice(1).map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((cell, cellIndex) => (
+                <td key={cellIndex}>{cell}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+
+  function searchTable(col: string, value: string): JSX.Element {
+    if (!currentData) return <p>No CSV loaded</p>;
+
+    const header = currentData[0];
+    let colIndex = -1;
+
+    // Check if col is a number
+    if (!isNaN(Number(col))) {
+      colIndex = Number(col);
+    } else {
+      // Check if col is a column name
+      // Convert both to lowercase for case-insensitive comparison
+      colIndex = header.map((h) => h.toLowerCase()).indexOf(col.toLowerCase());
+    }
+
+    if (colIndex === -1 || colIndex >= header.length) {
+      return <p>Invalid column specified</p>;
+    }
+
+    // Convert the value to lowercase for case-insensitive search
+    const lowerCaseValue = value.toLowerCase().trim();
+
+    // Filter rows based on whether the cell contains the search value
+    const matchingRows = currentData.filter((row) =>
+      row[colIndex].toLowerCase().includes(lowerCaseValue)
+    );
+
+    if (matchingRows.length === 0) {
+      return <p>No matching rows found</p>;
+    }
+
+    return (
+      <table border={1} className="centered-table">
+        <thead>
+          <tr>
+            {header.map((h, index) => (
+              <th key={index}>{h}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {matchingRows.map((row, rowIndex) => (
             <tr key={rowIndex}>
               {row.map((cell, cellIndex) => (
                 <td key={cellIndex}>{cell}</td>
